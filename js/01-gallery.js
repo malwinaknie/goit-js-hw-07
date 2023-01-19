bef66d6073675cff6c4d20d84f46d34ae4b1800d
Malwina Knie <malwinaknie@wp.pl>
2023-01-20 00:43:38 +0100

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,11 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const pictures = document.querySelector('gallery');
+
+pictures.forEach ((picture) => {
+    picture.addEventListener('click', (e) => {
+
+    })
+});
