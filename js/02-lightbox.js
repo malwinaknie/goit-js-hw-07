4176c08786145d99cc1b4c8154a75de6e09e9120
Malwina Knie <malwinaknie@wp.pl>
2023-01-24 02:38:55 +0100

    .

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((picture) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${picture.original}">
+    <img
+      class="gallery__image"
+      src="${picture.preview}"
+      alt="${picture.description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
