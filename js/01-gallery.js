97663f1cf1b178695a531c4ed4fed494f0f7192e
Malwina Knie <malwinaknie@wp.pl>
2023-01-24 01:51:25 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const pictures = document.querySelector('gallery');
+const gallery = document.querySelector('.gallery');
 
-pictures.forEach ((picture) => {
-    picture.addEventListener('click', (e) => {
-
-    })
+galleryItems.forEach((picture) => {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+   `<div class="gallery__item">
+  <a class="gallery__link" href="${picture.original}">
+    <img
+      class="gallery__image"
+      src="${picture.preview}"
+      data-source="${picture.original}"
+      alt="${picture.description}"
+    />
+  </a>
+</div>`
+  );
 });
+
+gallery.addEventListener("click", imageModal);
+
+function imageModal(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+      `);
+
+  instance.show();
+
+  document.addEventListener('keydown', e => {
+    if(e.key === 'Escape') {
+      instance.close();
+    }
+  })
+}
